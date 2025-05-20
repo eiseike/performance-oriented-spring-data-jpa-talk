97ce57e8a330bee9995ff790fac36c15341e36f8
Ádám Balázs <adam.balazs@appsfactory.de>
2025-05-20 09:32:41 +0200

    fix-4 the problem: RegisterBankTransferUseCaseTests.executes() => 2 find, 1 save = 6 db operations

--- src/main/java/com/example/RegisterBankTransferUseCase.java
+++ src/main/java/com/example/RegisterBankTransferUseCase.java
@@ -24,5 +24,16 @@ public class RegisterBankTransferUseCase {
 
         BankTransfer bankTransfer = new BankTransfer(bankTransferId, reference, sender, receiver, amount);
         bankTransferRepository.save(bankTransfer);
+
+        /*
+        Hibernate: select a1_0.id,a1_0.first_name,a1_0.iban,a1_0.last_name from account a1_0 where a1_0.id=?
+Hibernate: select a1_0.id,a1_0.first_name,a1_0.iban,a1_0.last_name from account a1_0 where a1_0.id=?
+Hibernate: select bt1_0.id,bt1_0.value,bt1_0.currency_code,r1_0.id,r1_0.first_name,r1_0.iban,r1_0.last_name,bt1_0.reference,s1_0.id,s1_0.first_name,s1_0.iban,s1_0.last_name,bt1_0.state from bank_transfer bt1_0 left join account r1_0 on r1_0.id=bt1_0.receiver_id left join account s1_0 on s1_0.id=bt1_0.sender_id where bt1_0.id=?
+Hibernate: select a1_0.id,a1_0.first_name,a1_0.iban,a1_0.last_name from account a1_0 where a1_0.id=?
+Hibernate: select a1_0.id,a1_0.first_name,a1_0.iban,a1_0.last_name from account a1_0 where a1_0.id=?
+Hibernate: insert into bank_transfer (value,currency_code,receiver_id,reference,sender_id,state,id) values (?,?,?,?,?,?,?)
+
+        is baaaaaad
+         */
     }
 }
